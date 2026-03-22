66b920123326cb5465fdb1ac4a2b42d57251e6f6
yujiezheng <yujie70338@gmail.com>
2026-03-22 14:38:45 +0800

    fix: correct formatting of policy ARN in EKS module

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -133,7 +133,7 @@ module "eks" {
 
       policy_associations = {
         cluster_admin = {
-          policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             type = "cluster"
           }
